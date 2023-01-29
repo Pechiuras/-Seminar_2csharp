289fb1c79552367554d7660b402ac8caa4de1da3
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-29 21:25:21 +0500

    task12

---
+++ Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿//task 12 (Кратность чисел и вывод остатка от деления)
+Console.Clear();
+Console.Write("Введите число 1: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n % m == 1)
+ {
+    Console.Write($"Число не кратно, остаток {n % m}");
+ }
+else
+{
+      Console.Write("Число кратно");  
+}
+
