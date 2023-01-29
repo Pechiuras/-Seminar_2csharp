dab2d7d13c4a7b1109272dec4c3df815e9d75916
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-29 20:48:43 +0500

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Выбор случайного числа из отрезка и вывод наибольшей цифры числа.
+Console.Clear();
+int n = new Random().Next(10, 100); // [10, 99]
+Console.WriteLine($"Случайное число: {n}"); 
+int n1 = n / 10;
+int n2 = n % 10;
+
+if (n1 > n2)
+      Console.WriteLine(n1);
+else
+      Console.WriteLine(n2);     
+
+ 
+
+
+
+
+
