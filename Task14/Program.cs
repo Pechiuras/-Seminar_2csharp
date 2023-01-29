653efa4fe2f968cc580739af2c7e418b103505ec
Aleksandr Pechiuras <alpechura@yandex.ru>
2023-01-29 21:45:20 +0500

    task14

---
+++ Task14/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 161 == 0)
+ {
+    Console.Write($"Число кратно");
+ }
+else
+{
+      Console.Write("Число не кратно");  
+}
